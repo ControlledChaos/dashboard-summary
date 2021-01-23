19e3259f036d031475c40946f65604845c7b9f0a
ControlledChaos <greg@ccdzine.com>
2021-01-22 23:18:36 -0800

    Default widget content filters.

--- views/default-widget.php
+++ views/default-widget.php
@@ -18,18 +18,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
+// Post types heading.
+$heading_types = apply_filters(
+	'ds_default_widget_heading_types',
+	__( 'Content Types', DS_DOMAIN )
+);
+
+// Taxonomies heading.
+$heading_taxes = apply_filters(
+	'ds_default_widget_heading_taxes',
+	__( 'Classification', DS_DOMAIN )
+);
+
 do_action( 'ds_default_widget_before' );
 
 ?>
 <div id="ds-default-widget" class="ds-widget ds-default-widget">
 
 	<section class="ds-widget-section">
-		<h3><?php _e( 'Content Types', DS_DOMAIN ); ?></h3>
+		<h3><?php echo $heading_types; ?></h3>
 		<?php Classes\summary()->post_types_list(); ?>
 	</section>
 
 	<section class="ds-widget-section">
-		<h3><?php _e( 'Content Organization', DS_DOMAIN ); ?></h3>
+		<h3><?php echo $heading_taxes; ?></h3>
 		<?php Classes\summary()->taxonomies_list(); ?>
 	</section>
 
