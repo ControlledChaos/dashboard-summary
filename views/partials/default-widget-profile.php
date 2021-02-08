cade63dbdf486e73833ec95029d24dcd45928bfc
ControlledChaos <greg@ccdzine.com>
2021-02-08 13:30:02 -0800

    Profile tab text.

--- views/partials/default-widget-profile.php
+++ views/partials/default-widget-profile.php
@@ -19,20 +19,63 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // System section heading.
-$heading = apply_filters(
+$heading_profile = apply_filters(
 	'ds_default_widget_heading_profile',
-	__( 'Your Profile', DS_DOMAIN )
+	sprintf(
+		'<h3 class="screen-reader-text">%s</h3>',
+		__( 'Your Profile', DS_DOMAIN )
+	)
 );
 
 // System section description.
-$description = apply_filters(
+$description_profile = apply_filters(
 	'ds_default_widget_description_profile',
 	sprintf(
 		'<p class="description">%s</p>',
-		__( 'Following are details about your account.', DS_DOMAIN )
+		__( 'Details about your account.', DS_DOMAIN )
+	)
+);
+
+// Personal information section heading.
+$heading_user_bio = apply_filters(
+	'ds_default_widget_heading_user_bio',
+	__( 'Personal Information', DS_DOMAIN )
+);
+
+// Personal information description.
+$description_user_bio = apply_filters(
+	'ds_default_widget_description_user_bio',
+	sprintf(
+		'<p class="description">%s</p>',
+		__( 'Your personal information is as follows. Some of this information may be displayed publicly.', DS_DOMAIN )
+	)
+);
+
+// Personal options section heading.
+$heading_user_options = apply_filters(
+	'ds_default_widget_heading_user_options',
+	__( 'Personal Options', DS_DOMAIN )
+);
+
+// Personal options description.
+$description_user_options = apply_filters(
+	'ds_default_widget_description_user_options',
+	sprintf(
+		'<p class="description">%s</p>',
+		__( 'A quick review of your options for using this website.', DS_DOMAIN )
 	)
 );
 
 ?>
-<h4><?php echo $heading; ?></h4>
-<?php echo $description; ?>
+<?php echo $heading_profile; ?>
+<?php echo $description_profile; ?>
+
+<div class="ds-widget-divided-section ds-widget-profile-section">
+	<h4><?php echo $heading_user_bio; ?></h4>
+	<?php echo $description_user_bio; ?>
+</div>
+
+<div class="ds-widget-divided-section ds-widget-profile-section">
+	<h4><?php echo $heading_user_options; ?></h4>
+	<?php echo $description_user_options; ?>
+</div>
