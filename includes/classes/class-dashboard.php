14d9df344bd9bd9e1059aefc30b7c12fdf74fda3
ControlledChaos <greg@ccdzine.com>
2021-02-09 10:15:44 -0800

    User notification colors.

--- includes/classes/class-dashboard.php
+++ includes/classes/class-dashboard.php
@@ -31,6 +31,9 @@ class Dashboard {
 
 		// Print admin scripts to head.
 		add_action( 'admin_print_scripts', [ $this, 'admin_print_scripts' ], 20 );
+
+		// Print admin styles to head.
+		add_action( 'admin_print_styles', [ $this, 'admin_print_styles' ], 20 );
 	}
 
 	/**
@@ -63,5 +66,29 @@ class Dashboard {
 
 		// Script to fill base64 background images with current link colors.
 		echo '<script type="text/javascript">var _dashboard_svg_icons = ' . wp_json_encode( $colors->user_colors() ) . ";</script>\n";
+	}
+
+	/**
+	 * Print admin styles
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string
+	 */
+	public function admin_print_styles() {
+
+		// Instantiate the User_Colors class.
+		$user_colors = new User_Colors;
+
+		// Get user notification colors.
+		$notify_color      = $user_colors->user_notify_colors();
+		$notify_background = $notify_color['background'];
+		$notify_text       = $notify_color['text'];
+
+		$style  = '<style>';
+		$style .= '#dashboard-widgets #ds-default-widget .ds-widget-update-count { background-color: ' . $notify_background . '; color: ' . $notify_text . '; }';
+		$style .= '</style>';
+
+		echo $style;
 	}
 }
--- includes/classes/class-user-colors.php
+++ includes/classes/class-user-colors.php
@@ -86,4 +86,171 @@ class User_Colors {
 		// Apply a filter for custom color schemes.
 		return apply_filters( 'ds_user_colors', $colors );
 	}
+
+	/**
+	 * User notification colors
+	 *
+	 * Used to print a style block for update count
+	 * colors in the default wdget, depending on the
+	 * user's color scheme preference. The color
+	 * likely does not match any in the color scheme
+	 * array so it is defined here by the color scheme
+	 * slug.
+	 *
+	 * Accounts for the Admin Color Schemes plugin and
+	 * a filter is applied for custom admin themes.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string Returns the hex code of a matching color.
+	 */
+	public function user_notify_colors() {
+
+		// Get the name of the user's color scheme.
+		$scheme = get_user_option( 'admin_color' );
+
+		// Modern scheme.
+		if ( 'modern' == $scheme ) {
+			$color = [
+				'background' => '#3858e9',
+				'text'       => '#ffffff'
+			];
+
+		// Light scheme.
+		} elseif ( 'light' == $scheme ) {
+			$color = [
+				'background' => '#d64e07',
+				'text'       => '#ffffff'
+			];
+
+		// 80's Kid scheme.
+		} elseif ( '80s-kid' == $scheme ) {
+			$color = [
+				'background' => '#43db2a',
+				'text'       => '#ffffff'
+			];
+
+		// Adderley scheme.
+		} elseif ( 'adderley' == $scheme ) {
+			$color = [
+				'background' => '#bde7f0',
+				'text'       => '#216bce'
+			];
+
+		// Aubergine scheme.
+		} elseif ( 'aubergine' == $scheme ) {
+			$color = [
+				'background' => '#d97042',
+				'text'       => '#ffffff'
+			];
+
+		// Blue scheme.
+		} elseif ( 'blue' == $scheme ) {
+			$color = [
+				'background' => '#e1a948',
+				'text'       => '#ffffff'
+			];
+
+		// Coffee scheme.
+		} elseif ( 'coffee' == $scheme ) {
+			$color = [
+				'background' => '#9ea476',
+				'text'       => '#ffffff'
+			];
+
+		// High Contrast Blue scheme.
+		} elseif ( 'contrast-blue' == $scheme ) {
+			$color = [
+				'background' => '#9d2f4d',
+				'text'       => '#ffffff'
+			];
+
+		// Cruise scheme.
+		} elseif ( 'cruise' == $scheme ) {
+			$color = [
+				'background' => '#d2ac1f',
+				'text'       => '#ffffff'
+			];
+
+		// Ectoplasm scheme.
+		} elseif ( 'ectoplasm' == $scheme ) {
+			$color = [
+				'background' => '#d46f15',
+				'text'       => '#ffffff'
+			];
+
+		// Flat scheme.
+		} elseif ( 'flat' == $scheme ) {
+			$color = [
+				'background' => '#d35401',
+				'text'       => '#ffffff'
+			];
+
+		// Kirk scheme.
+		} elseif ( 'kirk' == $scheme ) {
+			$color = [
+				'background' => '#bd3854',
+				'text'       => '#fefcf7'
+			];
+
+		// Lawn scheme.
+		} elseif ( 'lawn' == $scheme ) {
+			$color = [
+				'background' => '#456a7f',
+				'text'       => '#ffffff'
+			];
+
+		// Midnight scheme.
+		} elseif ( 'midnight' == $scheme ) {
+			$color = [
+				'background' => '#69a8bb',
+				'text'       => '#ffffff'
+			];
+
+		// Ocean scheme.
+		} elseif ( 'ocean' == $scheme ) {
+			$color = [
+				'background' => '#aa9d88',
+				'text'       => '#ffffff'
+			];
+
+		// Primary scheme.
+		} elseif ( 'primary' == $scheme ) {
+			$color = [
+				'background' => '#f48236',
+				'text'       => '#ffffff'
+			];
+
+		// Seashore scheme.
+		} elseif ( 'seashore' == $scheme ) {
+			$color = [
+				'background' => '#73340f',
+				'text'       => '#f8f6f1'
+			];
+
+		// Sunrise scheme.
+		} elseif ( 'sunrise' == $scheme ) {
+			$color = [
+				'background' => '#ccaf0b',
+				'text'       => '#ffffff'
+			];
+
+		// Vinyard scheme.
+		} elseif ( 'vinyard' == $scheme ) {
+			$color = [
+				'background' => '#ba8752',
+				'text'       => '#ffffff'
+			];
+
+		// The default and remaining native schemes.
+		} else {
+			$color = [
+				'background' => '#f56e28',
+				'text'       => '#ffffff'
+			];
+		}
+
+		// The array of colors.
+		return apply_filters( 'user_notify_colors', $color );
+	}
 }
